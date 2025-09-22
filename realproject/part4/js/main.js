05b7601dfeda3ac26219e2b7e1e482e1256acfb7
Zack Bonner <zackbonner@Zacks-MacBook-Pro.local>
2025-09-22 09:12:58 -0400

    fixing pages

---
+++ realproject/part4/js/main.js
@@ -0,0 +1,5 @@
+const menuToggle = document.getElementById("menu-toggle");
+const mainNav = document.getElementById("main-nav");
+menuToggle.addEventListener('click', () => {
+    mainNav.classList.toggle('nav-open');
+});

3f39b34d49517899752a40d11766cf3d8f07fa9c
Zachary Bonner <zbonner@email.sc.edu>
2025-10-28 11:38:18 -0400

    changed style

---
+++ sleevenotes-react-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello from Zack's React App!</h1> {This is my custom react app}
+      </header>
+    </div>
+  );
+}
+
+export default App;
